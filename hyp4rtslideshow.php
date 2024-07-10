3c09502ec04c50590d424e05050732e1797d5033
hyppolite <hyppolitak@gmail.com>
2024-07-10 04:12:00 +0100

    Add plugin file header

---
+++ hyp4rtslideshow.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * WordPress Plugin
+ *
+ * @package HypSlideshow
+ */
+
+/**
+ * Plugin Name: Hyp Slideshow
+ * Plugin URI: https://github.com/JuniorTak
+ * Description: A plugin to generate a slideshow for users to add into their websites
+ * Version: 1.0
+ * Author: Hyppolite Takoua Foduop
+ * Author URI: https://www.hyppolitetakouafoduop.online
+ * License: GPLv2 or later
+ * Text Domain: hypslideshow
+ */
