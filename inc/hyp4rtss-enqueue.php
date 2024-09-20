28d918965a67a080ae3b267a22156548b5409063
hyppolite <hyppolitak@gmail.com>
2024-09-20 18:41:39 +0100

    Enqueue styles and scripts to load in the admin for plugin setting page

---
+++ inc/hyp4rtss-enqueue.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Enqueue styles and scripts
+ *
+ * @package HypSlideshow
+ */
+
+// Action to load scripts in the admin.
+add_action( 'admin_enqueue_scripts', 'hypslideshow_admin_enqueue' );
+/**
+ * Define styles and scripts to load in the admin
+ */
+function hypslideshow_admin_enqueue() {
+	global $pagenow;
+	// Get the current page.
+    $current_page = $_GET['page'] ?? '';
+	// Check both current page base and slug.
+	if ( $pagenow == 'admin.php' && $current_page == 'hypslideshow' ) {
+		// Enqueue WordPress media scripts.
+		wp_enqueue_media();
+		// Enqueue styles.
+		wp_enqueue_style( 'hypss-jquery-ui-style', plugin_dir_url( __FILE__ ) . '../lib/jquery/1.13.2/jquery-ui.css', array(), '1.0.0', 'all' );
+		wp_enqueue_style( 'hypss-sortable-style', plugin_dir_url( __FILE__ ) . '../lib/sortable/sortable.css', array(), '1.0.0', 'all' );
+		// Enqueue scripts.
+		wp_enqueue_script( 'hypss-jquery-sortable', plugin_dir_url( __FILE__ ) . '../lib/jquery/jquery-3.6.0.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'hypss-jquery-ui-js', plugin_dir_url( __FILE__ ) . '../lib/jquery/1.13.2/jquery-ui.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'hypss-sortable-js', plugin_dir_url( __FILE__ ) . '../lib/sortable/sortable.js', array( 'jquery' ), '1.0.0', false );
+	}
+}
